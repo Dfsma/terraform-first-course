105bd7756640994433ba8ff87fc89c964f2d2e97
dfsma <diegof.df58@gmail.com>
2022-02-17 14:56:30 -0300

    uso de la variable user_data para tareas o script automaticas cuando inicie la instancia

--- crear-cluster-ec2/main.tf
+++ crear-cluster-ec2/main.tf
@@ -7,6 +7,14 @@ provider "aws" {
 resource "aws_instance" "mi_servidor" { #en la pagina de aws podremos ver lás amis y sus valores .
     ami = "" #valor de la ami (imagen) ej: ami-03101223fasc3c .
     instance_type = "" #tipo de instacia ej: t2.micro .
+
+    #se utiliza para transmitir datos de usuario a la instancia que se puede utilizar para realizar
+    #tareas de configuracion automáticas e incluso ejecutar scripts cuando se inicie la instancia
+    user_data = <<-EOF             
+                #!/bin/bash         
+                echo "¡Desde Terraform!" > index.html
+                nohup busybox httpd -f -p 8080 &&
+                EOF
 }
 
 
@@ -16,3 +24,7 @@ resource "aws_instance" "mi_servidor" { #en la pagina de aws podremos ver lás a
 #                  y a partir de eso, nos desplegara una lista con las acciones y recursos que va aplicar.
 # terraform apply  = (Despliegue) creara un plan y preguntará si quiere realizar los cambios.
 #########################################
+
+###Informacion que aparece en los resumenes del apply o plan###
+# forces replacement = quiere decir que eliminara el recurso y va a crear uno nuevo.
+###############################################################
